d93ed6a4ee73f838472c2f8be9eb5cb3c98e4d20
H.N.Jayalath <hirunijayalath74@gmail.com>
2026-07-26 15:04:17 +0530

    add PostgreSQL adapter for Prisma and update dependencies

---
+++ backend/database/prismaClient.ts
@@ -0,0 +1,15 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.js";
+import Pg from "pg";
+
+const connectionString =process.env.DATABASE_URL as string;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing in .env");
+}
+
+const pool = new Pg.Pool({connectionString});
+const adapter =new PrismaPg(pool); 
+
+export const prisma =new PrismaClient({adapter})
