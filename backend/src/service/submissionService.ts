da2f0b848c5ffb40e620dee3ee71e33d82a9b9be
H.N.Jayalath <hirunijayalath74@gmail.com>
2026-08-10 17:21:30 +0530

    feat: remove duplicate submission repository and service files

--- backend/src/service/submissionService.ts
+++
@@ -1,6 +0,0 @@
-import { getAllSubmissions } from "../repository/submissionRepository.js";
-
-export async function getAllSubmissionsService() {
-    const submissions = await getAllSubmissions();
-    return submissions;
-}
