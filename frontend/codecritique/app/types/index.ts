0e0720d9a7a37b23c9348d5a69b0aa0ae30fc652
H.N.Jayalath <hirunijayalath74@gmail.com>
2026-08-17 22:02:15 +0530

    Add review ID page and update types

--- frontend/codecritique/app/types/index.ts
+++ frontend/codecritique/app/types/index.ts
@@ -27,8 +27,14 @@ export type ReviewCriteria = {
 };
 
 export type ReviewRating = {
+  id: number;
   criteriaId: number;
   score: number;
+    criteria: {
+      id: number;
+      name:string;
+    }
+  
 };
 
 export type Review = {
